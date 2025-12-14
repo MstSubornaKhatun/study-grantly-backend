69a6a1b17906896a0fe4effde488d231ff727585
suborna33 <152749142+suborna33@users.noreply.github.com>
2025-12-14 13:11:25 +0600

    Polished some section

--- checkEnv.js
+++ checkEnv.js
@@ -1,5 +1,5 @@
 require('dotenv').config();
 
-// env ফাইল থেকে কি পড়ছে?
+// env 
 console.log('FB_SERVICE_KEY exists?', !!process.env.FB_SERVICE_KEY);
 console.log('First 40 chars:', (process.env.FB_SERVICE_KEY || '').slice(0, 40));
